9abb6dca7da039dec0807a01c7135d871c6d5e58
crystalDf <chendong333@gmail.com>
2017-01-07 00:53:03 +0800

    Upgrade to API Level 25

--- app/src/main/java/com/star/geoquiz/Question.java
+++ app/src/main/java/com/star/geoquiz/Question.java
@@ -26,4 +26,5 @@ public class Question {
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
     }
+
 }
--- app/src/main/java/com/star/geoquiz/QuizActivity.java
+++ app/src/main/java/com/star/geoquiz/QuizActivity.java
@@ -3,8 +3,6 @@ package com.star.geoquiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -144,25 +142,4 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
